b2639ef8ee9ed2d5c553ceb41221d90c16c77a55
micgarcia <micgarcia9185@gmail.com>
2022-10-15 15:09:45 -0700

    Created login form routes and views, added join-club routes and views, added list of member messages including the logic to handle if people are in the club (i.e. can see user names on messages)

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const Users = require("./models/users.js");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +25,46 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    Users.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err)
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+    })
+  })
+)
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+})
+
+passport.deserializeUser(function(id, done) {
+  Users.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+app.use(session({ secret: process.env.SECRET, resave: false, saveUnitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+})
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 const session = require("express-session");
@@ -6,17 +7,32 @@ const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 const { body, check, validationResult } = require('express-validator');
 const Users = require('../models/users.js');
+const Messages = require('../models/messages.js');
+const { DateTime } = require("luxon");
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Messages.find({})
+    .populate('sender')
+    .exec(function (err, messages) {
+      if (err) {
+        return next(err)
+      }
+      res.render('index', {
+        title: 'Members Only',
+        user: req.user,
+        messages: messages
+      });
+    })
 });
 
+// GET sign up form
 router.get('/sign-up', function(req, res, next) {
   res.render("sign-up")
 });
 
+// POST sign up form
 router.post('/sign-up',
   body("first_name", "First Name must not be empty.")
     .trim()
@@ -73,6 +89,104 @@ router.post('/sign-up',
         }
         res.redirect('/');
       })
+    })
+  }
+)
+
+// GET log-in form
+router.post("/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+
+// GET secret password join page/form
+router.get("/join", (req, res) => {
+  res.render('join', {
+    user: req.user
+  })
+})
+
+// POST join form
+router.post('/join',
+  body("secret", "Secret must not be empty")
+    .trim()
+    .isLength({min:1})
+    .escape(),
+  check('secret', 'Entered Code does not match Secret Code')
+    .trim()
+    .custom((value) => value === process.env.CODE),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("join", {
+        user: req.user,
+        errors: errors.array()
+      });
+      return;
+    }
+
+    const user = new Users({
+      first_name: req.user.first_name,
+      last_name: req.user.last_name,
+      username: req.user.username,
+      password: req.user.password,
+      member_status: 'vip',
+      _id: req.user.id
+    })
+
+    Users.findByIdAndUpdate(req.user.id, user, {}, (err, theuser) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+    })
+  }
+);
+
+// GET log out
+router.get('/log-out', (req, res, next) => {
+  req.logout(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
+// GET send message
+router.get('/send', (req, res, next) => {
+  res.render('send', {
+    user: req.user
+  })
+})
+
+// POST send message
+router.post('/send',
+  body("message", "Message must not be empty.")
+    .trim()
+    .isLength({min:1})
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('send', {
+        user: req.user,
+        errors: errors.array()
+      })
+    }
+    const message = new Messages({
+      title: req.body.title,
+      text: req.body.message,
+      timestamp: DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS),
+      sender: req.user
+    }).save((err) => {
+      if (err) {
+        return next(err)
+      }
+      res.redirect('/');
     })
   }
 )
